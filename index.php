dccfcc18c082d74043006643fcb4d0c9fdabb700
Traxs-YT <brieuc.tabordon@gmail.Com>
2021-06-24 19:52:38 +0200

    ajout html

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('data/liste_transactions.php');
+require_once('templates/liste.php');
---
+++ templates/liste.php
@@ -0,0 +1,19 @@
+<?php
+require_once('data/liste_transactions.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Examen ADB - Liste des transactions </title>
+</head>
+<body>
+<h1>Liste des transactions</h1>
+<ul>
+    <li>
+
+    </li>
+</ul>
+</body>
+</html>
